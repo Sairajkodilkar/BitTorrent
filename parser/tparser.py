7dafd5015660d61044f22fe6a1261e04a1e5cabc
Sairaj Kodilkar <skodilkar7@gmail.com>
2021-09-27 00:13:01 +0530

    Parser completed

--- parser/tparser.py
+++ parser/tparser.py
@@ -1,109 +1,107 @@
 '''
 6:string
-i3e
-l4:spam3:egge
+i3e l4:spam3:egge
 d3:cowi3ee
 '''
 
+import struct
+import sys
+
+class BencodingError(Exception):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
 class Token:
 
-    INT_TYPE = 'i'
-    STRING_TYPE = 's'
-    DICT_TYPE  = 'd'
-    LIST_TYPE = 'l'
-    END_TYPE = 'e'
-    EOF_TYPE = 'f'
+    INT_TYPE = b'i'
+    STRING_TYPE = b's'
+    DICT_TYPE  = b'd'
+    LIST_TYPE = b'l'
+    END_TYPE = b'e'
+    EOF_TYPE = b'f'
 
     def __init__(self, value, ttype):
-        self.token = value
+        self.value = value
         self.type = ttype
         pass
 
 class TorrentParser:
-    def parser(self, filename)->list:
-        '''
-        States:
-            1) specifier
-            2) and element
-        Functions:
-            1) parse dictionary
-            2) parse list
-            3) parse string
-            4) parse integer
-        '''
-        '''
-        call get next in the will loop 
-        get next will get the next token
-        depending on the next token call 
-                read string, 
-                read readdictionary,
-                readlist,
-                readinteger
-        '''
+
+    def parse(self, file)->list:
+        bencodingList = []
+        while True:
+            token = self.getnext(file)
+            if(token.type == Token.EOF_TYPE):
+                break
+            else:
+                bencodingList.append(token.value)
+
+        return bencodingList
+
 
     def getnext(self, file):
-            '''
-            read character 
-            if(integer): read till ":" and then readstring
-            if(i): readinteger till e
-            if(l): readlist
-            if(d): readdictionary
-            if(e): e
-            '''
             ch = file.read(1)
-            if(ch.isdigit()):
-                digits = ch
+            if(ch >= b'0' and ch <= b'9'):
+                digits = ch.decode()
                 while(True):
                     ch = file.read(1)
-                    if(ch == ':'):
+                    if(ch == b':'):
                         break
-                    elif(not ch.isdigit()):
+                    elif(not (ch >= b'0' and ch <= b'9')):
                         raise BencodingError("Expected Digit got {}".format(ch))
-                    digits += ch
+                    digits += ch.decode()
                 strlen = int(digits)
                 string = self.readstring(file, strlen)
                 return Token(string, Token.STRING_TYPE)
 
-            elif(ch == 'i'):
+            elif(ch == Token.INT_TYPE):
                 integer = self.readinteger(file)
                 return Token(integer, Token.INT_TYPE)
-            elif(ch == 'l'):
+            elif(ch == Token.LIST_TYPE):
                 ll = self.readlist(file)
                 return Token(ll, Token.LIST_TYPE)
-            elif(ch == 'd'):
+            elif(ch == Token.DICT_TYPE):
                 dictionary = self.readdictionary(file)
                 return Token(dictionary, Token.DICT_TYPE)
-            elif(ch == 'e'):
+            elif(ch == Token.END_TYPE):
                 return Token(None, Token.END_TYPE)
+            elif(not ch):
+                return Token(None, Token.EOF_TYPE)
+            else:
+                raise BencodingError(f"Unexpected Type specifier: {ord(ch)}")
 
 
     def readstring(self, file, strlen):
         if(strlen < 0):
             return ''
         string = file.read(strlen)
-        if(len(string) != strlne):
+        if(len(string) != strlen):
             raise BencodingError("Bad String Encoding")
+        return string
     
     def readinteger(self, file):
         integerString = ''
         start = True
+        zeroFlag = False
         while(True):
             ch = file.read(1)
-            if(ch == 'e'):
+            if(ch == b'e'):
                 if(start):
                     raise BencodingError("Empty integer in bencoding")
                 else:
                     break
             elif(not ch):
                 raise BencodingError("Unexpected EOF")
-            elif(not ch.isdigit() and not (start and ch == '-')):
+            elif(not (ch >= b'0' and ch <= b'9') and not (start and ch == b'-')):
                 raise BencodingError("Unexpected character")
             elif(zeroFlag):
                 raise BencodingError("Cannot have a digit after 0")
-            if(ch == '0' and start):
+            if(ch == b'0' and start):
                 zeroFlag = True
             start = False
-            integerString += ch
+            integerString += ch.decode()
 
         return int(integerString) 
 
@@ -113,14 +111,14 @@ class TorrentParser:
 
         while(True):
             key = self.getnext(file)
-            if(key.type == token.EOF_TYPE):
+            if(key.type == Token.EOF_TYPE):
                 raise BencodingError("Unexpected EOF")
             if(key.type == Token.END_TYPE):
                 break
             value = self.getnext(file)
             if(key.type == Token.END_TYPE):
                 raise BencodingError("Dictionary Cannot have Empty value field")
-            dictionary[key.value] = value.value
+            dictionary[key.value.decode()] = value.value
 
         return dictionary
 

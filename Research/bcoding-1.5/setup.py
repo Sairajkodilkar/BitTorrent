5dfca289ab03bd316339704b14b0a7841db118e6
Sairaj Kodilkar <skodilkar7@gmail.com>
2021-10-14 04:31:59 +0530

    Add research module

---
+++ Research/bcoding-1.5/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+	name='bcoding',
+	version='1.5',
+	py_modules=['bcoding',],
+	description='bencoding & -decoding library',
+	author='Phil Schaf',
+	author_email='flying-sheep@web.de',
+	license='MIT',
+	url='http://flying-sheep.github.com/bcode',
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.2',
+		'Programming Language :: Python :: 3.3',
+		'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+	])
